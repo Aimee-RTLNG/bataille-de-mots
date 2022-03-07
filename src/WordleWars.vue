e7e02ab36e72a74e0cf19ce325aa610fb7ce7baa
Aimee RITLENG <aimeeritleng@outlook.com>
2022-03-07 20:14:42 +0100

    feat(music): add global music player

--- src/WordleWars.vue
+++ src/WordleWars.vue
@@ -221,12 +221,84 @@ function createEmojiScore (successGrid: string) {
   resultString += '\n' + successGrid
   return resultString + '\n\nhttps://wleb.fr/bataille-de-mots'
 }
+
+// Player
+let is_playing = false;
+function toggleAudio(state : boolean){
+  if(state) {
+    document.getElementById('player').play();
+  } else {
+    document.getElementById('player').pause();
+  }
+  is_playing = state;
+}
+
 </script>
 
 <template>
   <ExampleWrapper>
+
+    <div id="music">
+        <audio loop volume=".2" id="player">
+          <source src="/music/motus-music.mp3" type="audio/mpeg">
+        </audio> 
+        <div class="flex">
+            <svg
+              class="mr-5"
+              width="50"
+              height="50"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M17 21C15.8954 21 15 20.1046 15 19V15C15 13.8954 15.8954 13 17 13H19V12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12V13H7C8.10457 13 9 13.8954 9 15V19C9 20.1046 8.10457 21 7 21H3V12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12V21H17ZM19 15H17V19H19V15ZM7 15H5V19H7V15Z"
+                fill="currentColor"
+              />
+            </svg>
+          <button class="button-icon" id="play-music" @click="toggleAudio(true)">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21ZM12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23Z"
+                fill="currentColor"
+              />
+              <path d="M16 12L10 16.3301V7.66987L16 12Z" fill="currentColor" />
+            </svg>
+          </button>
+          <button class="button-icon" id="pause-music" @click="toggleAudio(false)">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9 9H11V15H9V9Z" fill="currentColor" />
+              <path d="M15 15H13V9H15V15Z" fill="currentColor" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12ZM21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                fill="currentColor"
+              />
+            </svg>
+          </button>
+      </div>
+    </div>
+    
     <Header/>
     <div class="transition-wrapper">
+      
       <div v-if="gameState === GameState.CONNECTING" id="connecting">
         <div class="flex flex-col items-center justify-center text-2xl">
           <i class="gg-spinner-two" width="20"></i>
